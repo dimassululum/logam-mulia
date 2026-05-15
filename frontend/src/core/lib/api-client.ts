7de977f0792ecf6846970b0050a7d07385fa2467
Dimas Sul'Ulum <dimassululum@MacBook-Air-Dimas.local>
2026-05-15 23:56:26 +0700

    fix api routes

--- frontend/src/core/lib/api-client.ts
+++ frontend/src/core/lib/api-client.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const baseURL =
+  `${process.env.NEXT_PUBLIC_API_URL}/api`
+  || 'http://localhost:5000/api';
 
 export const apiClient = axios.create({
   baseURL,
